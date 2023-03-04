ee5fc37433611e65954034a9f04ef929cb099417
ReDay Zarra <redayzarra@gmail.com>
2023-03-04 09:57:05 -0500

    Setting up server file

--- exercise-project/backend/server.js
+++ exercise-project/backend/server.js
@@ -1,1 +1,14 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(post, () => {
+  console.log('Server is running on port: ${port}');
+});
